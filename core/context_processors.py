a4636fc592430c40f73ad3ef5ce46700ea40a9ed
Delton Mukoya Kuya <deltonmukoyakuya@gmail.com>
2026-02-11 13:32:34 +0300

    Restore direct Splash access in context processor

--- core/context_processors.py
+++ core/context_processors.py
@@ -9,13 +9,7 @@ from .models_splash import Splash
 def site_settings(request):
     """
     Add site settings to template context.
-    # Safe fetch for splash screen to avoid 500 errors if table doesn't exist yet
-    try:
-        active_splash = Splash.get_active()
-    except Exception:
-        active_splash = None
-
     return {
         'site_settings': SiteSettings.get_settings(),
-        'splash': active_splash
+        'splash': Splash.get_active()
     }
